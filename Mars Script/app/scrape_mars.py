6d6946a052ffee5367384f64b07865c8778c1ceb
Nico Boyajian <njboyajian@gwu.edu>
2020-03-12 12:05:36 -0400

    changes

--- Mars Script/app/scrape_mars.py
+++ Mars Script/app/scrape_mars.py
@@ -18,13 +18,14 @@ def scrape_all():
     # Run all scraping functions and store in dictionary.
     data = {
         "news_title": news_title,
-        
+
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "hemispheres": hemispheres(browser),
         "weather": twitter_weather(browser),
         "facts": mars_facts(),
         "last_modified": dt.datetime.now()
+        
     }
 
     # Stop webdriver and return data
